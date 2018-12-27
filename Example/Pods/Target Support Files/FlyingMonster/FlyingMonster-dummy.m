ced62583e912eccc82fe32b984cdcb9e56ee2a9d
lin charles <charleslin826@gmail.com>
2018-12-27 20:15:58 +0800

    Feat. Animated Monster & rocket join us

---
+++ Example/Pods/Target Support Files/FlyingMonster/FlyingMonster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FlyingMonster : NSObject
+@end
+@implementation PodsDummy_FlyingMonster
+@end
---
+++ Example/Pods/Target Support Files/Pods-FlyingMonster_Tests/Pods-FlyingMonster_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlyingMonster_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FlyingMonster_Tests
+@end
